c32fe83745c4250a756b175a7ac5792b98aed425
= <=>
2021-06-09 15:05:59 +0300

    Exercise 36

---
+++ Exercise36.py
@@ -0,0 +1,14 @@
+'''
+36. Write a Python program to replace a given tag with whatever's inside a given tag. 
+
+'''
+
+from bs4 import BeautifulSoup
+markup = '<a href="https://w3resource.com/">Python exercises.<i>w3resource.com</i></a>'
+soup = BeautifulSoup(markup, "lxml")
+a_tag = soup.a
+print("Original markup:")
+print(a_tag)
+a_tag.i.unwrap()
+print("\nAfter unwrapping:")
+print(a_tag)
