78483aa0bf16f781bc003c64f8913b1082b158ea
= <=>
2021-06-09 13:46:24 +0300

    Exercise 32

---
+++ Exercise32.py
@@ -0,0 +1,13 @@
+'''
+32. Write a Python program to extract a tag or string from a given tree of html document.
+
+'''
+
+from bs4 import BeautifulSoup
+html_content = '<a href="https://w3resource.com/">Python exercises<i>w3resource</i></a>'
+soup = BeautifulSoup(html_content, "lxml")
+print("Original Markup:")
+print(soup.a)
+i_tag = soup.i.extract()
+print("\nExtract i tag from said html Markup:")
+print(i_tag)
