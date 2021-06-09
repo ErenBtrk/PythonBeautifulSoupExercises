2711bd647b9e22e6e110a224502f38d4c8d63834
= <=>
2021-06-09 13:38:13 +0300

    Exercise 27

---
+++ Exercise27.py
@@ -0,0 +1,13 @@
+'''
+27. Write a Python program to add to a tag's contents in a given html document. 
+
+'''
+
+from bs4 import BeautifulSoup
+html_doc = '<a href="http://example.com/">HTML<i>w3resource.com</i></a>'
+soup = BeautifulSoup(html_doc, "lxml")
+print("\nOriginal Markup:")
+print(soup.a)
+soup.a.append("CSS")
+print("\nAfter append a text in the new link:")
+print(soup.a)
