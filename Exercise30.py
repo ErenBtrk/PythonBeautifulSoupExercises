6c46bf9653fec77ee338ff856e51f62d2b1853f8
= <=>
2021-06-09 13:43:48 +0300

    Exercise 30

---
+++ Exercise30.py
@@ -0,0 +1,14 @@
+'''
+30. Write a Python program to insert tags or strings immediately after specified tags or strings.
+
+'''
+
+from bs4 import BeautifulSoup
+soup = BeautifulSoup("<b>w3resource.com</b>", "lxml")
+print("Original Markup:")
+print(soup.b)
+tag = soup.new_tag("i")
+tag.string = "Python"
+print("\nNew Markup, after inserting the text:")
+soup.b.string.insert_after(tag)
+print(soup.b)
